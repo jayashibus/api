f7ea48b246367069a39fdb188f8ba4b53c04d656
jayashibus <jayashibu@gmail.com>
2023-04-14 23:45:02 +0530

    DB config files added

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,10 +1,12 @@
+const asyncHandler = require("express-async-handler");
+
 //Get All contacts
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all the contacts" });
-};
+});
 
 //Create contact
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log(req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -12,22 +14,22 @@ const createContact = (req, res) => {
     throw new Error("All fields are mandatory");
   }
   res.status(201).json({ message: "Create contacts" });
-};
+});
 
 //Update contact
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Edit contact for ${req.params.id}` });
-};
+});
 
 //Update contact
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Edit contact for ${req.params.id}` });
-};
+});
 
 //Delete contact
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
